29d15e159fb7b231eec6ab9ce920e7aecc6ed6b1
mishk0 <mokrushin.ms@yandex.ru>
2016-02-04 16:44:46 +0300

    fix export name

--- index.js
+++ index.js
@@ -4,21 +4,31 @@ var path = require('path');
 var querystring = require('querystring');
 
 module.exports = function() {
-    var parsedQuery = querystring.parse(this.query.slice(1));
-    var commonPath = parsedQuery.common;
-    var objPath = commonPath + '.obj';
+    if (this.query) {
+        var parsedQuery = querystring.parse(this.query.slice(1));
+        var commonPath = parsedQuery.common;
+
+        compileCommonModule(commonPath);
+    }
+
+    var js = yate.compile(this.resourcePath).js;
+
+    return 'exports["yate-' + path.basename(this.resourcePath, '.yate') + '"] = (function(){' + js + '})();';
+
+};
+
+function compileCommonModule(path) {
+    var objPath = path + '.obj';
 
     var isExist = fs.existsSync(objPath);
 
     if (!isExist) {
-        yate.compile(commonPath);
+        yate.compile(path);
     }
 
-    var obj = JSON.parse( fs.readFileSync(objPath, 'utf8') );
-    yate.modules = {};
-    yate.modules['common'] = obj;
+    var obj = JSON.parse(fs.readFileSync(objPath, 'utf8'));
 
-    var js = yate.compile(this.resourcePath).js;
-    return 'exports.yate_' + path.basename(this.resourcePath, '.yate') + ' = (function(){' + js + '})();';
-
-};
+    yate.modules = {
+        'common': obj
+    };
+}
